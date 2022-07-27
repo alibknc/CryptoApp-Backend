f85ba419f8293065bd4bcaf8c63d97dfa68886c1
Ali Bakinc <muhammedbknc@gmail.com>
2022-07-27 12:16:09 +0300

    slf4j configuration done

--- src/main/java/com/project/cryptoapp/controller/ApiController.java
+++ src/main/java/com/project/cryptoapp/controller/ApiController.java
@@ -1,10 +1,12 @@
 package com.project.cryptoapp.controller;
 
 import com.project.cryptoapp.services.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class ApiController {
@@ -18,17 +20,20 @@ public class ApiController {
 
     @GetMapping("/")
     public String getHello(){
+        log.debug("getHello is called");
         return "Hello World!";
     }
 
     @GetMapping("/ticker")
     public HashMap<String, Object> getTickerBySymbol(@RequestParam String symbol, HttpServletRequest request){
+        log.debug("getTickerBySymbol is called");
         //return apiService.getTickerBySymbol(symbol, request);
         return webSocketService.getTickerBySymbol(symbol, request);
     }
 
     @GetMapping("/info")
     public HashMap<String, Object> getSymbolList(){
+        log.debug("getSymbolList is called");
         return apiService.getSymbolList();
     }
 }
